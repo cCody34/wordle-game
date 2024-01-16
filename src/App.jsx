0a060427f24e6543603ab42d109bbb547e1c6eeb
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2024-01-16 14:56:40 +0000

    set up header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <h1>Wordle</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
